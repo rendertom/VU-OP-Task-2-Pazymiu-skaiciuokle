10c22904301f8fbe4aa13232dcd620c461d13d70
Tomas Sinkunas <Tomas@rendertom.com>
2021-02-19 13:32:00 +0200

    chore(file): add RND.hpp file

---
+++ RND.hpp
@@ -0,0 +1,16 @@
+// Adopted from https://repl.it/@AleksandrIgumen/AtsKlase#RandInt.hpp
+
+#include <random>
+
+namespace RND {
+
+std::random_device rd;
+std::mt19937 mt(rd());
+
+int getIntegerInRange(int min, int max) {
+  std::uniform_int_distribution<int> uid(min, max);
+
+  return uid(mt);
+}
+
+}  // namespace RND
