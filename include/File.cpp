c46af77a5ac4d2c4531d5118c4bfe02f668e01ae
renderTom <Tomas@rendertom.com>
2021-02-20 19:25:11 +0200

    chore(File): add more methods

--- include/File.cpp
+++ include/File.cpp
@@ -1,8 +1,8 @@
 #include "File.hpp"
 
-bool File::doesFileExist(const string &filePath) {
-  ifstream file(filePath);
-  return file.good();
+bool File::fileExists(const string &filePath) {
+  struct stat info;
+  return stat(filePath.c_str(), &info) == 0;
 }
 
 stringstream File::getBuffer(const string &filePath) {
@@ -18,4 +18,70 @@ stringstream File::getBuffer(const string &filePath) {
   file.close();
 
   return buffer;
+}
+
+vector<string> File::getFilenamesInFolder(const string &folderPath, const string &extension) {
+  DIR *directory = opendir(folderPath.c_str());
+  struct dirent *entry;
+  vector<string> fileNames;
+
+  if (directory != NULL) {
+    while ((entry = readdir(directory)) != NULL) {
+      string fileName = entry->d_name;
+      if (String::hasSuffix(fileName, extension)) {
+        fileNames.push_back(fileName);
+      }
+    }
+    closedir(directory);
+  }
+
+  return fileNames;
+}
+
+bool File::isFile(const string &filePath) {
+  struct stat info;
+  if (stat(filePath.c_str(), &info) == 0) {
+    if (info.st_mode & S_IFREG) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool File::isFolder(const string &filePath) {
+  struct stat info;
+  if (stat(filePath.c_str(), &info) == 0) {
+    if (info.st_mode & S_IFDIR) {
+      return true;
+    }
+  }
+  return false;
+}
+
+string File::selectFileInFolder(const string &folderPath, const string &extension) {
+  if (!fileExists(folderPath)) {
+    cout << "Folder \"" << folderPath << "\" does not exist." << endl;
+  } else if (!isFolder(folderPath)) {
+    cout << "\"" << folderPath << "\" is not a folder." << endl;
+  } else {
+    vector<string> fileNames = getFilenamesInFolder(folderPath, extension);
+    int numFiles = fileNames.size();
+    if (numFiles == 0) {
+      cout << "Folder \"" << folderPath << "\" contains no files with extension \"" << extension << "\"." << endl;
+    } else if (numFiles == 1) {
+      return fileNames[0];
+    } else {
+      std::sort(fileNames.begin(), fileNames.end());
+
+      cout << "Folder \"" << folderPath << "\" contains multiple files. Please select one:" << endl;
+      for (int i = 0; i < numFiles; i++) {
+        cout << i + 1 << ": " << fileNames[i] << endl;
+      }
+
+      int index = Console::promptForInt("Please select a file:", 1, numFiles);
+      return fileNames[index - 1];
+    }
+  }
+
+  return "";
 }
--- include/File.hpp
+++ include/File.hpp
@@ -1,17 +1,30 @@
 #pragma once
 
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include <fstream>  // std::ifstream
 #include <iostream>
 #include <sstream>  // std::stringstream
 #include <string>
+#include <vector>
+
+#include "Console.hpp"
+#include "String.hpp"
 
 using std::cout;
 using std::endl;
 using std::ifstream;
 using std::string;
 using std::stringstream;
+using std::vector;
 
 namespace File {
-  bool doesFileExist(const string &);
+  bool fileExists(const string &);
   stringstream getBuffer(const string &);
+  vector<string> getFilenamesInFolder(const string &, const string &);
+  bool isFile(const string &);
+  bool isFolder(const string &);
+  string selectFileInFolder(const string &, const string &);
 }
